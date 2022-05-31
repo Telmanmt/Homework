374f150f1d1914da8cd87823d903037587e191cb
Telmanmt <106176742+Telmanmt@users.noreply.github.com>
2022-05-31 10:01:46 +0400

    Task3

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿string stroka1 = "adidas";
+int adidasnumber = 4;
+int adidascost = 200;
+string stroka2 = "firma"; 
+string stroka3 = "ayaqqabi sayi";
+string stroka4 = "birinin qiymeti";
+int stroka5 = adidasnumber * adidascost;
+string stroka6 = "hamisinin birlikde qiymeti";
+Console.WriteLine(stroka2);
+Console.WriteLine(stroka1);
+Console.WriteLine(stroka3);
+Console.WriteLine(adidasnumber);
+Console.WriteLine(stroka4);
+Console.WriteLine(adidascost);
+Console.WriteLine(stroka6);
+Console.WriteLine(stroka5);
+
+
+string stroka7 = "gucci";
+int guccinumber = 2;
+int guccicost = 1000;
+int stroka8 = guccicost * guccinumber;
+Console.WriteLine(stroka2);
+Console.WriteLine(stroka7);
+Console.WriteLine(stroka3);
+Console.WriteLine(guccinumber);
+Console.WriteLine(stroka4);
+Console.WriteLine(guccicost);
+Console.WriteLine(stroka6);
+Console.WriteLine(stroka8);
+
+
+
+
+string stroka9 = "nike";
+int nikenumber = 1;
+int nikecost = 150;
+int stroka10 = nikenumber * nikecost;
+Console.WriteLine(stroka2);
+Console.WriteLine(stroka9);
+Console.WriteLine(stroka3);
+Console.WriteLine(nikenumber);
+Console.WriteLine(stroka4);
+Console.WriteLine(nikecost);
+Console.WriteLine(stroka6);
+Console.WriteLine(stroka10);
